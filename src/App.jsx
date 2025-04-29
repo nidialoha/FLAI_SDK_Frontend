bd17f91d439437cde2ec62a95bff437e086953a4
nidialoha <nidialoha@gmail.com>
2025-04-29 13:20:53 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import LoginButton from "./Login";
+import LogoutButton from "./Logout";
+import Profile from "./Profile";
+
+function App() {
+  return (
+    <div>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import "./index.css";
+import App from "./App.jsx";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <Auth0Provider
+    domain="dev-gwpms22afgh3xnex.us.auth0.com"
+    clientId="MvihB0y1by8CE9aIuycZtGdvTIYadAyI"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <App />
+  </Auth0Provider>
+);
